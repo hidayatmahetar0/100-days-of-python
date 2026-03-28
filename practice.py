6f9967ef3b6ce2c76710fe542ade67c259a818b1
hidayatmahetar0 <hidayatmahetar818@gmail.com>
2026-03-28 16:54:35 +0530

    prectice

--- practice.py
+++ practice.py
@@ -1,62 +1,119 @@
-# Practice 1
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+# # Practice 1
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
 
-sum_result = num1 + num2
-difference = num1 - num2
-product = num1 * num2
+# sum_result = num1 + num2
+# difference = num1 - num2
+# product = num1 * num2
 
-if num2 != 0:
-    division = num1 / num2
-else:
-    division = "Cannot divide by zero"
+# if num2 != 0:
+#     division = num1 / num2 
+# else:
+#     division = "Cannot divide by zero"
 
-print("Sum:", sum_result)
-print("Difference:", difference)
-print("Product:", product)
-print("Division:", division)
-
-
-# Practice 2
-num = int(input("Enter a number: "))
-
-if num > 0:
-    print("This Number is Positive")
-elif num < 0:
-    print("This Number is Negative")
-else:
-    print("Zero")
+# print("Sum:", sum_result)
+# print("Difference:", difference)
+# print("Product:", product)
+# print("Division:", division)
 
 
-# Practice 3
-age = int(input("Enter age: "))
+# # Practice 2
+# num = int(input("Enter a number: "))
 
-if age >= 18:
-    print("Adult")
-else:
-    print("Minor")
-
-
-# Practice 4
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-if num1 > num2:
-    print("First number is larger")
-elif num2 > num1:
-    print("Second number is larger")
-else:
-    print("Both numbers are equal")
+# if num > 0:
+#     print("This Number is Positive")
+# elif num < 0:
+#     print("This Number is Negative")
+# else:
+#     print("Zero")
 
 
-# Practice 5
-num = int(input("Marks: "))
+# # Practice 3
+# age = int(input("Enter age: "))
 
-if num >= 90:
-    print("Grade is A")
-elif num >= 86:
-    print("Grade is B")
-elif num >= 74:
-    print("Grade is C")
-else:
-    print("Fail")
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+
+# # Practice 4
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# if num1 > num2:
+#     print("First number is larger")
+# elif num2 > num1:
+#     print("Second number is larger")
+# else:
+#     print("Both numbers are equal")
+
+
+# # Practice 5
+# num = int(input("Marks: "))
+
+# if num >= 90:
+#     print("Grade is A")
+# elif num >= 86:
+#     print("Grade is B")
+# elif num >= 74:
+#     print("Grade is C")
+# else:
+#     print("Fail")
+
+
+# # user thi input levu
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# # operation puchvu
+# op = input("Enter operation (+, -, *, /): ")
+
+# # condition check kari ne result aapvu
+# if op == "+":
+#     print("Result:", num1 + num2)
+
+# elif op == "-":
+#     print("Result:", num1 - num2)
+
+# elif op == "*":
+#     print("Result:", num1 * num2)
+
+# elif op == "/":
+#     if num2 != 0:
+#         print("Result:", num1 / num2)
+#     else:
+#         print("Error: Division by zero!")
+
+# else:
+#     print("Invalid operation")
+
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+
+# if a >= b and a >= c:
+#     print("Biggest number is:", a)
+
+# elif b >= a and b >= c:
+#     print("Biggest number is:", b)
+
+# else:
+#     print("Biggest number is:", c)
+
+
+# year = int(input("Enter a year: "))
+
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("Leap Year")
+# else:
+#     print("Not a Leap Year")
+
+
+# num = int(input("Enter Number: "))
+
+# if (num % 5 == 0):
+#     print("Divided by 5")
+# else:
+#     print("Not devided")
